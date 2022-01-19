042181bc9cda0146d5a9701f96ca04ee7d55ab6b
joeldevel <christianjoel.developer@gmail.com>
2022-01-18 21:07:29 -0300

    adds styles and images

--- src/App.js
+++ src/App.js
@@ -1,37 +1,43 @@
 import React, {useState, useEffect} from 'react';
 
-import InputTextArea from './components/InputTextArea';
-import OutputTextArea from './components/OutputTextArea';
 import marked from 'marked';
 
 import {initialText} from './initial_text';
 
 function App() {
 
-  const [inputText, setInputText] = useState(null);
-  const [outputText, setOutputText] = useState(null);
+  const [inputText, setInputText] = useState("");
+  const [outputText, setOutputText] = useState("");
 
   const  handleInputText = (e) => {
-    setInputText(e.target.value);
+      setInputText(e.target.value);
   };
 
   useEffect( () => {
-    marked.setOptions({breaks:true});
-    const o = inputText? marked(inputText): "";
-    setOutputText(o);
+      marked.setOptions({breaks:true});
+      const o = inputText? marked(inputText): "";
+      setOutputText(o);
   });
 
   useEffect(()=>{
-    setInputText(initialText);
+      setInputText(initialText);
   },[]);
 
   return (
-    <div className="App">
-      <h1>Write here</h1>
-      <textarea onChange={handleInputText} id="editor" cols="100" rows="20" value={inputText}/>
-
-      <h2>Preview it</h2>
-      {outputText?<div dangerouslySetInnerHTML={{__html: outputText}} id="preview"></div>: <div id="preview"></div>}
+    <div className="app">
+        <div className="panel">
+            <h2 className="panel-title">source</h2>
+            <textarea
+                onChange={handleInputText}
+                id="editor"
+                value={inputText}
+                className="editor"
+            />
+        </div>
+        <div className="panel">
+            <h2 className="panel-title">preview</h2>
+            {outputText?<div className="preview-panel" dangerouslySetInnerHTML={{__html: outputText}} id="preview"></div>: <div id="preview"></div>}
+        </div>
     </div>
   );
 }
